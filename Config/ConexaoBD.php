d67d388d1afbc3ea96ea814942e061e6f81efe7c
italoangelo13 <italoangelo.ti@gmail.com>
2019-12-05 19:02:57 -0300

    alteração no config de conexao

--- Config/ConexaoBD.php
+++ Config/ConexaoBD.php
@@ -3,5 +3,5 @@ header("Content-type:text/html;charset=utf8");
 define("server", "mysql:host=localhost;dbname=kingcar");
 define('user', 'root');
 define('senha', 'root');
-
+setlocale(LC_MONETARY,"pt_BR", "ptb");
 ?>
